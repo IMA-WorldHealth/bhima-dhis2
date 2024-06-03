b52d6eb9d28ec24d1710cd6690aaa7d30a1f4532
Ebed-meleck <ebedmeleckmakoso@gmail.com>
2024-06-03 12:09:33 +0100

    rumer bhima to dhis2

---
+++ README.md
@@ -0,0 +1,46 @@
+# BHIMA-DHIS2
+
+BHIMA-DHIS2 allows you to execute the data import script from BHIMA to DHIS2. Currently, this process is done manually while waiting for automation. Therefore, certain configurations need to be set only in the environment file. An .xls file containing information about the different molecules and their DHIS2 keys is required.
+
+## Prerequisites
+
+First, you need to have a version of BHIMA running on your PC.
+
+## Configuration
+
+In the environment file, configure the following variables:
+
+```env
+PATH_RUMER_FUNCTION='../bhima/path' -- the path where the rumer function file is located in BHIMA
+PATH_DB='../bhima/path' -- the database file
+START_DATE='2024-04-01' -- specify the start period
+END_DATE='2024-04-31' -- specify the end period
+
+DHIS2_SERVER_URL='https://test.dhis2.com' -- specify the sever url
+DHIS2_USERNAME='username'
+DHIS2_PASSWORD='password'
+
+DB_PORT=3306
+# DB_HOST is the MySQL server Host
+DB_HOST='localhost'
+# DB_USER is the MYSQL client user to connect to the server with
+DB_USER='db_user'
+# DB_PASS is the password for the MySQL user
+DB_PASS='db_password'
+# DB_NAME is the database name to connect to.
+DB_NAME='database'
+```
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun start
+```
+
+This project was created using `bun init` in bun v1.0.29. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ index.js
@@ -0,0 +1,201 @@
+require('dotenv').config();
+
+const _ = require('lodash');
+const path = require('path');
+const axios = require('axios');
+const moment = require('moment');
+const fs = require('fs');
+const xlsx = require('xlsx');
+
+const rumer = require(`${process.env.PATH_RUMER_FUNCTION}`);
+const db = require(`${process.env.PATH_DB}`);
+
+const DHIS2_SERVER_URL = process.env.DHIS2_SERVER_URL;
+const DHIS2_USERNAME = process.env.DHIS2_USERNAME;
+const DHIS2_PASSWORD =  process.env.DHIS2_PASSWORD;
+
+async function formatData(startDate, endDate, depot) {
+  try {
+    const pathLink = path.join(__dirname, './list_medicament_colonne_dhis2.xls');
+    const wb = xlsx.readFile(pathLink);
+    const ws1 = wb.SheetNames[0];
+    const ws2 = wb.SheetNames[1];
+    const listInventory = _.uniqBy(xlsx.utils.sheet_to_json(wb.Sheets[ws1]), 'DE_UID');
+    const colonnes = xlsx.utils.sheet_to_json(wb.Sheets[ws2]);
+    const result = [];
+
+    await depot.reduce(async (promise, item) => {
+      await promise;
+      const data = await rumer.getData({
+        depot_uuid : item.uuid,
+        start_date : startDate,
+        end_date : endDate,
+        condensed_report : 1,
+      });
+      const period = moment(startDate).format('YYYY-MM-DD').split('-');
+      if (data.data.configurationData.length > 0) {
+        data.data.configurationData.forEach((field) => {
+          listInventory.forEach((inv) => {
+            if (inv.DE_NAME === field.inventoryText) {
+              colonnes.forEach((col) => {
+                if (col.label === 'quantityOpening') {
+                  result.push({
+                    period: `${period[0]}${period[1]}`,
+                    orgUnit: item.dhis2_uid,
+                    dataElement: inv.DE_UID,
+                    categoryOptionCombo: col.uids,
+                    value: field.quantityOpening,
+                    storedBy: 'BHIMA SYSTEM',
+                    lastUpdated: moment(new Date()).format('YYYY-MM-DD'),
+                    comment: '',
+                    deleted: null,
+                    followup: false,
+                  });
+                }
+                if (col.label === 'quantityEnding') {
+                  result.push({
+                    period: `${period[0]}${period[1]}`,
+                    orgUnit: item.dhis2_uid,
+                    dataElement: inv.DE_UID,
+                    categoryOptionCombo: col.uids,
+                    value: field.quantityEnding,
+                    storedBy: 'BHIMA SYSTEM',
+                    lastUpdated: moment(new Date()).format('YYYY-MM-DD'),
+                    comment: '',
+                    deleted: null,
+                    followup: false,
+                  });
+                }
+                if (col.label === 'outQuantityConsumption') {
+                  result.push({
+                    period: `${period[0]}${period[1]}`,
+                    orgUnit: item.dhis2_uid,
+                    dataElement: inv.DE_UID,
+                    categoryOptionCombo: col.uids,
+                    value: field.outQuantityConsumption,
+                    storedBy: 'BHIMA SYSTEM',
+                    lastUpdated: moment(new Date()).format('YYYY-MM-DD'),
+                    comment: '',
+                    deleted: null,
+                    followup: false,
+                  });
+                }
+
+                if (col.label === 'quantityTotalEntry') {
+                  result.push({
+                    period: `${period[0]}${period[1]}`,
+                    orgUnit: item.dhis2_uid,
+                    dataElement: inv.DE_UID,
+                    categoryOptionCombo: col.uids,
+                    value: field.quantityTotalEntry,
+                    storedBy: 'BHIMA SYSTEM',
+                    lastUpdated: moment(new Date()).format('YYYY-MM-DD'),
+                    comment: '',
+                    deleted: null,
+                    followup: false,
+                  });
+
+                }
+                if (col.label === 'quantityTotalExit') {
+                  result.push({
+                    period: `${period[0]}${period[1]}`,
+                    orgUnit: item.dhis2_uid,
+                    dataElement: inv.DE_UID,
+                    categoryOptionCombo: col.uids,
+                    value: field.quantityTotalExit,
+                    storedBy: 'BHIMA SYSTEM',
+                    lastUpdated: moment(new Date()).format('YYYY-MM-DD'),
+                    comment: '',
+                    deleted: null,
+                    followup: false,
+                  });
+                }
+                if (col.label === 'outQuantityExit') {
+                  result.push({
+                    period: `${period[0]}${period[1]}`,
+                    orgUnit: item.dhis2_uid,
+                    dataElement: inv.DE_UID,
+                    categoryOptionCombo: col.uids,
+                    value: field.outQuantityExit,
+                    storedBy: 'BHIMA SYSTEM',
+                    lastUpdated: moment(new Date()).format('YYYY-MM-DD'),
+                    comment: '',
+                    deleted: null,
+                    followup: false,
+                  });
+                }
+                if (col.label === 'numStockOutDays') {
+                  result.push({
+                    period: `${period[0]}${period[1]}`,
+                    orgUnit: item.dhis2_uid,
+                    dataElement: inv.DE_UID,
+                    categoryOptionCombo: col.uids,
+                    value: field.numStockOutDays,
+                    storedBy: 'BHIMA SYSTEM',
+                    lastUpdated: moment(new Date()).format('YYYY-MM-DD'),
+                    comment: '',
+                    deleted: null,
+                    followup: false,
+                  });
+                }
+              });
+            }
+          });
+        });
+      }
+    }, Promise.resolve());
+    return _.uniqWith(result, _.isEqual);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function importToDhis2(data) {
+  try {
+    const form = {
+      dataValues : data,
+    };
+    fs.writeFileSync('./dhis2.json', JSON.stringify(form));
+    const response = await axios.post(
+      `${DHIS2_SERVER_URL}/api/dataValueSets?async=true&skipAudit=true`,
+      form,
+      {
+        auth : {
+          username : DHIS2_USERNAME,
+          password : DHIS2_PASSWORD,
+        },
+      },
+    );
+
+    console.log('Importation des données réussie:', response.data);
+  } catch (error) {
+    console.error(
+      'Erreur lors de l\'importation des données sur DHIS2:',
+      error.message,
+    );
+    throw error;
+  }
+}
+
+/**
+ * @method main
+ * @description This script allows you to send data from rumer bhima to
+ *  dhis2 for all the structures that are linked
+ */
+async function main() {
+  const sql = `SELECT * FROM depot WHERE dhis2_uid IS NOT NULL;`;
+  console.log('please wait');
+  try {
+    const depots = await db.exec(sql);
+    const startDate = process.env.START_DATE;
+    const endDate = process.env.END_DATE;
+    const data = await formatData(startDate, endDate, depots);
+    await importToDhis2(data);
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+main();
